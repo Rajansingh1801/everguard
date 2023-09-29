c059157ace234ce38a774a88eb90d50ac831d77a
Rajan singh <87349777+Rajansingh1801@users.noreply.github.com>
2023-09-29 13:28:32 +0530

    r1

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -4,6 +4,7 @@ import sleepgirl from "../../assets/images/about/girlsleep.png";
 
 const About = () => {
   const [scrollY, setScrollY] = useState(0);
+
   useEffect(() => {
     const handleScroll = () => {
       setScrollY(window.scrollY);
@@ -16,22 +17,30 @@ const About = () => {
     };
   }, []);
 
-  console.log(scrollY);
-
   let scaleFactorHeight = 0;
   let scaleFactorWidth = 0;
-  if (scrollY < 4722) {
-    scaleFactorHeight = 1 + scrollY * 0.00055;
-    scaleFactorWidth = 1 + scrollY * 0.0014;
-  } else if (scrollY > 4722) {
-    scaleFactorHeight = 3.5971;
-    scaleFactorWidth = 7.6108;
-  }
+  scaleFactorHeight = 1 + scrollY * 0.018;
+  scaleFactorWidth = 1 + scrollY * 0.02;
+
   console.log(scaleFactorHeight);
 
-  const divStyle = {
-    transform: `scale(${scaleFactorWidth}, ${scaleFactorHeight})`,
+  const borderScrollWidth = 1 + scrollY * 0.012;
+  const borderScrollHeight = 1 + scrollY * 0.019;
+  const rotate = 1 + scrollY * 0.029;
 
+  console.log("borderScrollWidth" + borderScrollWidth);
+  // console.log("Rotate is " + rotate);
+  console.log("scale Y" + scrollY);
+
+  const borderstyle = {
+    transform: `rotate(${rotate}deg)`,
+    height: `${35 + borderScrollHeight}vh`,
+    width: `${30 + borderScrollWidth}vw`,
+  };
+
+  const divStyle = {
+    height: `${15 + scaleFactorHeight}vh`,
+    width: `${15 + scaleFactorWidth}vw`,
     transition: "transform 0.2s ease",
   };
 
@@ -39,39 +48,14 @@ const About = () => {
     <>
       <section className="about">
         <div className="about-container flex items-center justify-center">
-          <div className="border flex items-center justify-center">
-            <img
-              src={sleepgirl}
-              alt="img"
-              className="sleepgirl"
-              style={divStyle}
-            />
-          </div>
+          <div
+            className="border flex items-center justify-center"
+            style={borderstyle}
+          ></div>
+          <div className="sleepgirl" style={divStyle} />
         </div>
         <div className="new1"></div>
 
-        {/* <div className="new7">
-          <div className="heading z-50">
-            <h2 className="text-white">No Sleep</h2>
-            <h2 className="text-white">No Energy</h2>
-          </div>
-        </div>
-        <div className="new8">
-          <div className="z-50 about-details">
-            <p className="text-white">
-              While other vitamin and shake options promise to fuel you up on
-              nutrients, only <br /> SIMPLIFI gets to the root of your health...
-            </p>
-            <h5 className="text-white">Your sleep!</h5>
-            <p className="text-white">
-              Without a rejuvenating night's sleep, your body can't rebuild and
-              recharge. Our <br /> scientifically formulated AM and PM takes
-              give you round the clock nutrient support <br /> to aid your body
-              in a full night's sleep. Plus, it tastes great and costs less than
-            </p>
-            <h5 className="text-white">$ 4 day.</h5>
-          </div>
-        </div> */}
         <div className="new9">
           <div className="new7">
             <div className="heading z-50">
@@ -94,7 +78,7 @@ const About = () => {
                 your body in a full night's sleep. Plus, it tastes great and
                 costs less than
               </p>
-              <h5 className="text-white">$ 4 day.</h5>
+              <h5 className="text-white">$4 day.</h5>
             </div>
           </div>
         </div>
